c80c38118c38cf902eb7c0111a50bdd366760c96
dcebeci <cebecidogukan@gmail.com>
2022-04-25 02:47:24 +0300

    chatfeed eklendi.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            heidht='100vh'
+            projectID="4a088490-e029-4ca4-b5d7-ec1e53e6b6c4"
+            userName="DC"
+            userSecret="Dcjora08."
+            renderChatFeed={( chatAppProps) => }
+
+        />
+    )
+
+
+
+
+}
+
+export default App;
