1d9059802532647158daacf1c426f52c82630488
dcebeci <cebecidogukan@gmail.com>
2022-04-26 23:57:53 +0300

    msj

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,5 +1,30 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
+
+
 const ChatFeed = (props) => {
-    console.log(props);
+    const { chats, activeChat, userName, messages} = props;
+
+    const chat = chat && chats[activeChat];
+
+   const renderMessages = () => {
+       const keys = Object.keys(messages);
+
+       return keys.map((key, index)=>{
+           const message = messages[key];
+           const lastMessageKey = index === 0 ? null : keys[index - 1]
+           const isMymessage = userName === message.sender.userName;
+           return(
+               <div key={'msg_${index}'} style={{width:'100%'}}>
+
+               </div>
+           )
+        })
+   }
+
+   renderMessages();
 
     return (
         <div>
